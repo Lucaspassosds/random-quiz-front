bb20b9a0377e5090bdea73420574d083f8d9f585
Lucas Passos <lucas.passos@avantia.com.br>
2021-08-04 14:53:16 -0300

    add quiztype integration

--- src/components/Menu.component.tsx
+++ src/components/Menu.component.tsx
@@ -1,22 +1,23 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { QuestionType } from '../models/interfaces';
 
+const apiUrl = 'http://localhost:3030';
 
 const Menu: React.FC = () => {
 
-    const [quizTypes, setQuizTypes] = useState<string[]>([]);
+    const [quizTypes, setQuizTypes] = useState<QuestionType[]>([]);
     useEffect(() => {
-        // async function fetchQuiz() {
-        //     const res = await axios.get('localhost:3030');
-        //     console.log(res);
-
-        // }
-        // fetchQuiz();
-        setQuizTypes(['Geral', 'Futebol', 'Games']);
+        async function fetchQuizTypes() {
+            const res = await axios.get(`${apiUrl}/types`);
+            const quizTypes: QuestionType[] = res.data;
+            setQuizTypes(quizTypes);
+        }
+        fetchQuizTypes();
     }, []);
 
-    const listQuizTypes = () => quizTypes.map(type => <a href={'/quiz/' + type} className="btn btn-blue">{type}</a>);
+    const listQuizTypes = () => quizTypes.map(type => <Link to={'/quiz/' + type.name} className="btn btn-blue">{type.name}</Link>);
 
     return (
         <div className="max-w-sm mx-auto bg-yellow-300 p-2 rounded-lg border-2 border-yellow-800">
